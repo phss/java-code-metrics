6b5b076e031b94beeed081a59758a1ec9f1e5228
Paulo Schneider <paulo.schneider@gmail.com>
2015-08-16 13:58:12 +0100

    Programmatically create config

--- src/main/java/com/phsshp/Main.java
+++ src/main/java/com/phsshp/Main.java
@@ -1,16 +1,13 @@
 package com.phsshp;
 
 import com.puppycrawl.tools.checkstyle.Checker;
-import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
 import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
-import com.puppycrawl.tools.checkstyle.PropertiesExpander;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Properties;
 
 public class Main {
 
@@ -19,8 +16,11 @@ public class Main {
         final Checker checker = new Checker();
 
         try {
-            final Configuration config = ConfigurationLoader.loadConfiguration(
-                    "checkstyle-metrics.xml", new PropertiesExpander(new Properties()));
+            final Configuration config = newConfig();
+
+            System.out.println("=== PRINTING METRICS ===");
+            printConfig(config);
+            System.out.println("=== END ===");
 
             final ClassLoader moduleClassLoader = Checker.class.getClassLoader();
             checker.setModuleClassLoader(moduleClassLoader);
@@ -36,5 +36,42 @@ public class Main {
         }
 
         System.out.println(errorCounter);
+    }
+
+    private static Configuration newConfig() {
+        DefaultConfiguration rootConfig = new DefaultConfiguration("Checker");
+        rootConfig.addAttribute("severity", "warning");
+
+        DefaultConfiguration fileLength = new DefaultConfiguration("FileLength");
+        fileLength.addAttribute("max", "0");
+        rootConfig.addChild(fileLength);
+
+        DefaultConfiguration treeWalker = new DefaultConfiguration("TreeWalker");
+        rootConfig.addChild(treeWalker);
+
+        DefaultConfiguration fileContent = new DefaultConfiguration("FileContentsHolder");
+        treeWalker.addChild(fileContent);
+
+        DefaultConfiguration fanOut = new DefaultConfiguration("ClassFanOutComplexity");
+        fanOut.addAttribute("max", "0");
+        treeWalker.addChild(fanOut);
+
+        DefaultConfiguration complexity = new DefaultConfiguration("CyclomaticComplexity");
+        complexity.addAttribute("max", "0");
+        treeWalker.addChild(complexity);
+
+        return rootConfig;
+    }
+
+    private static void printConfig(Configuration config) throws CheckstyleException {
+        System.out.println("NAME: " + config.getName());
+        for (String attribute : config.getAttributeNames()) {
+            System.out.println(attribute + ": " + config.getAttribute(attribute));
+        }
+        System.out.println("");
+
+        for (Configuration c : config.getChildren()) {
+            printConfig(c);
+        }
     }
 }
