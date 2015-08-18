69bb6392057c051766e34952efbcc36623b4be41
Paulo Schneider <paulo.schneider@gmail.com>
2015-08-18 12:59:43 +0100

    Only list java files

--- src/main/java/com/phsshp/FileLister.java
+++ src/main/java/com/phsshp/FileLister.java
@@ -2,6 +2,7 @@ package com.phsshp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +13,10 @@ public class FileLister {
     public List<File> list(String path) throws FileNotFoundException {
         File file = new File(path);
         if (file.exists()) {
-            return Arrays.asList(file);
+            if (file.getAbsolutePath().endsWith(".java")) {
+                return Arrays.asList(file);
+            }
+            return new ArrayList<>();
         }
         throw new FileNotFoundException(format("Path '%s' not found", path));
     }
--- src/test/java/com/phsshp/FileListerTest.java
+++ src/test/java/com/phsshp/FileListerTest.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertThat;
 
 public class FileListerTest {
@@ -16,7 +18,7 @@ public class FileListerTest {
     private final FileLister fileLister = new FileLister();
 
     @Test
-    public void listInexistentDirectoryReturnEmptyList() throws FileNotFoundException {
+    public void throwExceptionIfFileInPathNotFound() throws Exception {
         exception.expect(FileNotFoundException.class);
         exception.expectMessage("Path 'src/test/resources/nosuchdir' not found");
 
@@ -24,8 +26,15 @@ public class FileListerTest {
     }
 
     @Test
-    public void listSingleFile() throws FileNotFoundException {
-        assertThat(fileLister.list("src/test/resources/test-project/SomeFile.java"), contains(new File("src/test/resources/test-project/SomeFile.java")));
+    public void listSingleFile() throws Exception {
+        assertThat(fileLister.list("src/test/resources/test-project/SomeFile.java"),
+                contains(new File("src/test/resources/test-project/SomeFile.java")));
     }
+
+    @Test
+    public void emptyListForNonJavaFile() throws Exception {
+        assertThat(fileLister.list("src/test/resources/test-project/SomeFile.class"), is(empty()));
+    }
+
 
 }
