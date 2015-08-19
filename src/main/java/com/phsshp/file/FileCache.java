d722254ba0100ada86770e3e5e800aa2e053e470
Paulo Schneider <paulo.schneider@gmail.com>
2015-08-19 13:06:40 +0100

    Get file from cache based on absolute path

--- src/main/java/com/phsshp/file/FileCache.java
+++ src/main/java/com/phsshp/file/FileCache.java
@@ -2,10 +2,24 @@ package com.phsshp.file;
 
 import java.io.File;
 import java.nio.file.NoSuchFileException;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 public class FileCache {
 
+    private final Map<String, File> absolutePathLookup;
+
+    public FileCache(List<File> files) {
+        absolutePathLookup = files.stream().collect(toMap(File::getAbsolutePath, identity()));
+    }
+
     public File getByAbsolutePath(String path) throws NoSuchFileException {
+        if (absolutePathLookup.containsKey(path)) {
+            return absolutePathLookup.get(path);
+        }
         throw new NoSuchFileException(path);
     }
 }
--- src/test/java/com/phsshp/file/FileCacheTest.java
+++ src/test/java/com/phsshp/file/FileCacheTest.java
@@ -2,12 +2,27 @@ package com.phsshp.file;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FileCacheTest {
 
+    private final File testFile = new File("src/test/resources/test-project/SomeFile.java");
+    private final FileCache fileCache = new FileCache(Arrays.asList(testFile));
+
     @Test(expected = NoSuchFileException.class)
     public void throwsExceptionIfFileNotInCache() throws Exception {
-        new FileCache().getByAbsolutePath("src/no-such-file.txt");
+        fileCache.getByAbsolutePath("src/no-such-file.txt");
+    }
+
+    @Test
+    public void fetchesFileByAbsolutePath() throws Exception {
+        File file = fileCache.getByAbsolutePath(testFile.getAbsolutePath());
+
+        assertThat(file, equalTo(testFile));
     }
 }
