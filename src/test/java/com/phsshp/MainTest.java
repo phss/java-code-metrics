c82c2ee20e8ac4c60377a83e56d6ba6b4341449b
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-18 11:33:55 -0200

    Enhance test to actually measure fanout

--- src/test/java/com/phsshp/MainTest.java
+++ src/test/java/com/phsshp/MainTest.java
@@ -42,7 +42,7 @@ public class MainTest {
                 "file,size,cyclomatic_complexity,fanout_complexity",
                 "src/test/resources/test-project/SomeFile.java,40,11,0",
                 "src/test/resources/test-project/pkg1/AnotherInPackage1.java,4,0,0",
-                "src/test/resources/test-project/pkg1/InPackage1.java,6,1,0",
+                "src/test/resources/test-project/pkg1/InPackage1.java,8,1,1",
                 "src/test/resources/test-project/pkg2/InPackage2.java,10,2,0")));
     }
 
--- src/test/resources/test-project/pkg1/InPackage1.java
+++ src/test/resources/test-project/pkg1/InPackage1.java
@@ -1,6 +1,8 @@
 package testproject.pkg1;
 
 public class InPackage1 {
+    private AnotherInPackage1 someVar;
     public InPackage1() {
+        someVar = new AnotherInPackage1();
     }
 }
