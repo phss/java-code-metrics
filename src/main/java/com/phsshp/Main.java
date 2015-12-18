f5bc390fffd5264c691e82c1c3cf0a875aa667d5
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-18 11:00:06 -0200

    Sort report output by filename

--- src/main/java/com/phsshp/Main.java
+++ src/main/java/com/phsshp/Main.java
@@ -7,6 +7,7 @@ import com.phsshp.metrics.reporter.checkstyle.CheckstyleMetricsReporter;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -15,9 +16,12 @@ public class Main {
     public static void main(String args[]) throws Exception {
         List<File> files = new JavaFileLister().list(args[0]);
         MetricsReport report = new CheckstyleMetricsReporter().report(files);
+        List<FileMeasurements> sortedMeasurements = report.getMeasurements()
+                .stream().sorted((f1, f2) -> f1.getFile().compareTo(f2.getFile()))
+                .collect(Collectors.toList());
 
         System.out.println("file,size,cyclomatic_complexity");
-        for (FileMeasurements measurements : report.getMeasurements()) {
+        for (FileMeasurements measurements : sortedMeasurements) {
             System.out.println(format("%s,%d,%d",
                     measurements.getFile().getPath(),
                     measurements.getFileSize(),
--- src/test/java/com/phsshp/MainTest.java
+++ src/test/java/com/phsshp/MainTest.java
@@ -40,10 +40,10 @@ public class MainTest {
 
         assertThat(outContent.toString(), equalTo(linesString(
                 "file,size,cyclomatic_complexity",
-                "src/test/resources/test-project/pkg2/InPackage2.java,10,2",
-                "src/test/resources/test-project/pkg1/InPackage1.java,6,1",
+                "src/test/resources/test-project/SomeFile.java,40,11",
                 "src/test/resources/test-project/pkg1/AnotherInPackage1.java,4,0",
-                "src/test/resources/test-project/SomeFile.java,40,11")));
+                "src/test/resources/test-project/pkg1/InPackage1.java,6,1",
+                "src/test/resources/test-project/pkg2/InPackage2.java,10,2")));
     }
 
     private String linesString(String ...lines) {
